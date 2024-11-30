731bb75740244a71fcc5a8f73c983f0828f294c9
Worgzz333r <golubeckola@gmail.com>
2024-11-30 17:11:06 +0200

    Контейнеризація проєкту

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ import sqlite3
 from datetime import datetime
 
 def get_db_connection():
-    conn = sqlite3.connect('db.sqlite')
+    conn = sqlite3.connect('data/db.sqlite')
     conn.row_factory = sqlite3.Row
     return conn
 
